2d0f3beb5868ac413d49fc6a5a49ddfc312eab24
Ronald Glover <ron881212@gmail.com>
2019-11-14 12:04:28 -0500

    Finished making custom puzzle templates

--- src/pages/CreatePuzzle/index.js
+++ src/pages/CreatePuzzle/index.js
@@ -1,12 +1,12 @@
 import React, { useState, useRef, useEffect } from 'react';
 import NavBar from '../../components/Nav';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Button } from 'reactstrap';
 
 const CreatePuzzle = (props) => {
     const [gridNum, setGridNum] = useState([]);
     const [gridComplete, setGridComplete] = useState(false);
     const [dropdownOpen, setDropdownOpen] = useState(false);
-    const [confirmGrid, setConfirmGrid] = useState(null);
+    const [block, setBlock] = useState(false);
     const puzzleContainer = useRef(null)
     const toggle = () => setDropdownOpen(prevState => !prevState);
     let wordArray = []
@@ -15,20 +15,38 @@ const CreatePuzzle = (props) => {
     useEffect(()=> highlightGrid())
 
     const grid = () => {
-        console.log(gridNum.length)
         var gridStyle={
-            gridboxes: {display:'flex',
-            flexGrow: 2,
-            border:'1px solid black',
-            width: '100px'
-        }}
-        // boxSize.boxSet = gridNum.length
+            gridboxes: {
+                display:'flex',
+                flexGrow: 2,
+                border:'1px solid black',
+                backgroundColor: 'white',
+                fontSize: '70%',
+                margin:0,
+                padding:0
+            }
+        }
         if(gridNum.length == 16) gridStyle.gridboxes.width = 500 / 4 - 1 + 'px'
         else if(gridNum.length == 36) gridStyle.gridboxes.width = 500 / 6 - 1 + 'px'
         else if(gridNum.length == 64) gridStyle.gridboxes.width = 500 / 8 - 1 + 'px'
         else if(gridNum.length == 100) gridStyle.gridboxes.width = 500 / 10 - 1 + 'px'
         else if(gridNum.length == 0 || 1) gridStyle.gridboxes.width = '499px'
-        return gridNum.map(grids=> (<div key={grids} datatype={grids} style={gridStyle.gridboxes}> {grids} </div>))
+
+        return gridNum.map(grids=> (<input key={grids} 
+        onMouseOver={(here)=> { !block ?
+        here.currentTarget.style.border = '10px inset #5bc0de' :
+        here.currentTarget.style.border = '10px inset #292b2c'}}
+        onMouseOut={(here)=> {
+            !block ? here.currentTarget.style.border = '1px solid black' :
+            here.currentTarget.style.border = '1px solid black'
+            }}
+        datatype={grids} style={gridStyle.gridboxes} placeholder={grids}
+        onChange={handleChange}
+        onClick={(a)=> {
+            block ? a.target.style.backgroundColor = '#292b2c' : 
+            a.target.style.backgroundColor = 'white'}}
+        maxLength="1"
+        />))
     }
 
     const pickGridNumber = (num) => {
@@ -39,8 +57,8 @@ const CreatePuzzle = (props) => {
         }
         // setGridNum(gridNum.push(gridArray))
         // console.log(gridArray)
-        console.log(gridNum)    
-        console.log(typeof gridNum)
+        // console.log(gridNum)    
+        // console.log(typeof gridNum)
         // console.log(typeof gridArray)
         setGridComplete(true)
     }
@@ -48,6 +66,11 @@ const CreatePuzzle = (props) => {
         const highlightBox = puzzleContainer.current.childNodes
         highlightBox.onmouseover = ()=> this.style.backgroundColor = "blue"
         highlightBox.onmouseout = ()=> this.style.backgroundColor = "blue" 
+    }
+    const handleChange = (e) => {
+        e.target.value = e.target.value
+        console.log(e.target.value)
+        console.log(e.target)
     }
         return (
             <div>
@@ -58,6 +81,7 @@ const CreatePuzzle = (props) => {
                             { gridComplete ? grid() : null }
                         </div>
                     </div>
+                    <div style={{display:'flex',flexDirection:'column'}}>
                     <Dropdown isOpen={dropdownOpen} toggle={toggle}>
                       <DropdownToggle caret>
                       Number of Rows and Column
@@ -70,6 +94,8 @@ const CreatePuzzle = (props) => {
                         <DropdownItem onClick={()=>pickGridNumber(10)}>10</DropdownItem>
                       </DropdownMenu>
                     </Dropdown>
+                    <Button color="dark" onClick={()=> setBlock(!block)}>{!block ? 'BLOCK' : 'UNBLOCK'}</Button>
+                    </div>
                 </div>
             </div>
         );
@@ -90,11 +116,6 @@ const styles = {
         // borderRadius:'5%',
         marginTop:'5%'
     },
-    // gridboxes: {
-    //     display:'flex',
-    //     border:'1px solid black',
-    //     width: '100px'
-    // },
     containerTwo: {
         display:'flex',
         flexFlow: 'row wrap'
